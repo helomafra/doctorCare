c4827f35ff4589426ae4c76682bb84c2294f0196
Helô <heloisacmafra@gmail.com>
2022-05-06 17:33:43 -0300

    first-commit

---
+++ main.js
@@ -0,0 +1,85 @@
+window.addEventListener("scroll", onScroll);
+
+onScroll();
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+//innerHeight no console me diz qual a altura da viewport
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  //verificar se o inicio sessão passou da linha alvo
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  //verificando informações e dados da logica
+  console.log(
+    "O topo da seção chegou ou passou da linha?",
+    sectionTopReachOrPassedTargetLine
+  );
+
+  //verificar se o fim da sessão (base) passou linha alvo
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  // verificando informações e dados da logica
+  console.log("o final da seção passou da linha?", sectionEndPassedTargetLine);
+
+  //limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats,
+  #services,
+  #services header,
+  #services .cards,
+  #about,
+  #about header,
+  #about .content`);
